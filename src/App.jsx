184dc07e05f3c1d3c1be95610154580cf35a9ceb
Kyle Cardwell <kyle.e.cardwell@gmail.com>
2023-02-12 20:19:15 -0700

    app can search multiple products

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ProductForm from './components/ProductForm'
 import Product from './components/Product'
 
 function App() {
 
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    console.log('my Products', products)
+  }), [products]
 
   return (
     <div className="App">
-      <Product />
-      <Product />
+      <ProductForm setProducts={setProducts}/>
+      <div>Products
+
+        <div className='productsContainer'>
+          {products.map((spec) => {
+            return <Product spec={spec}/>
+          })}
+        </div>
+
+      </div>
     </div>
   )
 }
